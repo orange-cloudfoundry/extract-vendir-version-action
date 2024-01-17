77bda6586334ac77ca6a22b289c49494ce874b92
Olivier ORAND <olivier.orand@orange.com>
2024-01-17 15:21:00 +0000

    First version

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine
+
+RUN apk add git curl jq bash
+
+# renovate: datasource=github-releases depName=mikefarah/yq
+ENV YQ_VERSION="4.40.5"
+RUN echo "Installing yq version ${YQ_VERSION}" ; \
+    curl -L "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64" -o /usr/local/bin/yq && \
+    chmod +rx /usr/local/bin/yq && \
+    /usr/local/bin/yq --version
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo "*** Inputs ***"
+echo "  vendir_file: $INPUT_VENDIR_FILE"
+echo "  library: $INPUT_LIBRARY"
+echo "  type: $INPUT_TYPE"
+echo "  debug: $INPUT_DEBUG"
+
+
+echo "Vendir file:"
+ls -l $INPUT_VENDIR_FILE
+
+case $INPUT_TYPE in
+githubRelease)
+  filter=".githubRelease.tag"
+
+  ;;
+*)
+  echo "ERROR: undefined type"
+  exit 1
+esac
+
+printf "'" >filter.query
+printf '.directories[0].contents[] | select (.path=="'$INPUT_LIBRARY'") | '>>filter.query
+printf "$filter" >>filter.query
+printf "'" >>filter.query
+version=$(yq -r --from-file filter.query $INPUT_VENDIR_FILE)
+
+
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#environment-files
+echo "version=${version}"                     >> $GITHUB_OUTPUT
+
